c9f921af6bad4319ee2eef4cc8b1d1b3630ee2b8
Eric Dai <cridequeger23@gmail.com>
2025-04-07 18:13:02 -0600

    (SSR): Update configurations

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  assetPrefix: './',
+  transpilePackages: ["antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table"],
 };
 
 export default nextConfig;
